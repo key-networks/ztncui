d05963695b17f789cac917b7ad04c733f0294d72
key-networks <34238649+key-networks@users.noreply.github.com>
2021-12-22 19:43:17 +0800

    node16: Builds on node v16.13.1 (#79)
    * node16: Builds on node v16.13.1
    
    * node16: Builds on CentOS 8

--- build/build.sh
+++ build/build.sh
@@ -24,9 +24,9 @@ LICENSE='GPLv3'
 
 BINDINGGYP='node_modules/argon2/binding.gyp'
 
-NODE_VER='v14'
+NODE_VER='v16'
 
-if [ ! -f  /usr/lib/gcc/x86_64-redhat-linux/10/libstdc++.a ]; then
+if [ ! -f /usr/lib/gcc/x86_64-redhat-linux/8/libstdc++.a ]; then
   echo "You must install libstdc++-static"
   exit 1
 fi
@@ -75,7 +75,7 @@ if [ $? -ne 0 ]; then
 fi
 
 popd
-pkg -c ./package.json -t node14-linux-x64 bin/www -o $BUILD_DIR/ztncui
+pkg -c ./package.json -t node16-linux-x64 bin/www -o $BUILD_DIR/ztncui
 
 popd
 
